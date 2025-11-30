4ba04662312b2e166cd8bac4c416a171293ad306
LoggeL <hyper.xjo@gmail.com>
2025-11-30 18:37:45 +0100

    first commit

---
+++ 00/solve.py
@@ -0,0 +1,35 @@
+def solve1(lines):
+    pass
+
+
+def solve2(lines):
+    pass
+
+
+if __name__ == "__main__":
+    test1 = open("test1", "r").read().strip().split("\n")
+    test2 = open("test2", "r").read().strip().split("\n")
+    input = open("input", "r").read().strip().split("\n")
+
+    if test2 == [""]:
+        test2 = test1
+
+    test1_solution = 0
+    test2_solution = 0
+
+    test1_result = solve1(test1)
+    test2_result = solve2(test2)
+
+    print("Test 1: " + str(test1_result))
+    if test1_solution != test1_result:
+        print("Test 1 failed!")
+        exit(1)
+
+    print("Solution 1: " + str(solve1(input)))
+
+    print("Test 2: " + str(test2_result))
+    if test2_solution != test2_result:
+        print("Test 2 failed!")
+        exit(1)
+
+    print("Solution 2: " + str(solve2(input)))
