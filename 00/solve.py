ecf49f922621a7ddb8a572a31824df6531e63dbd
Johannes Stueber <joahnnes.a.stueber@basf.com>
2025-12-02 10:10:22 +0100

    whoops overwrote template

--- 00/solve.py
+++ 00/solve.py
@@ -1,47 +1,9 @@
 def solve1(lines):
-
-    dial = 50
-    score = 0
-
-    for line in lines:
-        direction, value = line[0], int(line[1:])
-        if direction == "L":
-            dial = (dial - value) % 100
-        elif direction == "R":
-            dial = (dial + value) % 100
-
-        # If the dial is at 0, increase score
-        if dial == 0:
-            score += 1
-
-    return score
+    pass
 
 
 def solve2(lines):
-
-    dial = 50
-    score = 0
-
-    for line in lines:
-        new_dial = None
-        direction, value = line[0], int(line[1:])
-
-        if direction == "L":
-            new_dial = dial - value
-        elif direction == "R":
-            new_dial = dial + value
-
-        if new_dial <= 0:
-            score += (-new_dial) // 100
-            if dial > 0: # if we started above 0, we crossed 0
-                score += 1
-        elif new_dial >= 100:
-            score += new_dial // 100
-
-        dial = new_dial % 100
-
-    # < 5900
-    return score
+    pass
 
 if __name__ == "__main__":
     test1 = open("test1", "r").read().strip().split("\n")
@@ -51,8 +13,8 @@ if __name__ == "__main__":
     if test2 == [""]:
         test2 = test1
 
-    test1_solution = 3
-    test2_solution = 6
+    test1_solution = 0
+    test2_solution = 0
 
     test1_result = solve1(test1)
     test2_result = solve2(test2)
